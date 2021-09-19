b5c7571304c5b615613a02485699a95559253b63
Alliix <delirioussoymilk@gmail.com>
2021-09-19 16:46:50 +0300

    use email

--- index.js
+++ index.js
@@ -2,19 +2,41 @@ import ObjectsToCsv from "objects-to-csv";
 import snoowrap from "snoowrap";
 import config from "./config.js";
 import cron from "node-cron";
+import nodemailer from "nodemailer";
+import express from "express";
+
+var app = express();
+
+var mail = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
 
 const removeSpecialChar = (str) => {
   if (str) return str.toString().replace(/\n/g, "").replace(",", "_");
 };
 
 const formatTime = (unix_timestamp) => {
-  let a = new Date(unix_timestamp * 1000);
-  let year = a.getFullYear();
-  let month = a.getMonth() + 1;
-  let date = a.getDate();
-  let hour = a.getHours();
-  let min = a.getMinutes();
-  let time = hour + ":" + min + " " + date + "-" + month + "-" + year;
+  let uDate = new Date(unix_timestamp * 1000);
+  let year = uDate.getFullYear();
+  let month = uDate.getMonth() + 1;
+  let date = uDate.getDate();
+  let hour = uDate.getHours();
+  let min = uDate.getMinutes();
+  let sec = uDate.getSeconds();
+  let time =
+    hour + ":" + min + ":" + sec + " " + date + "-" + month + "-" + year;
+  return time;
+};
+
+const formatDateNow = (now) => {
+  let year = now.getFullYear();
+  let month = now.getMonth() + 1;
+  let date = now.getDate();
+  let time = date + "-" + month + "-" + year;
   return time;
 };
 
@@ -26,34 +48,57 @@ const r = new snoowrap({
   password: config.password,
 });
 
-cron.schedule("0 0 * * *", function () {
-  r.getHot()
-    .fetchAll()
-    .then((posts) => {
-      return posts.filter((post) => post.selftext !== "");
-    })
-    .map((post) => {
-      return {
-        id: post.id,
-        author: post.author.name,
-        title: post.title,
-        selfText: removeSpecialChar(post.selftext),
-        date: formatTime(post.created_utc),
-        score: post.score,
-        subreddit: post.subreddit_name_prefixed,
-        numComments: post.num_comments,
-        spoiler: post.spoiler ? true : false,
-        nsfw: post.over_18 ? true : false,
-        isVideo: post.is_video,
-      };
-    })
-    .then(async (posts) => {
-      const csv = new ObjectsToCsv(posts);
-      const now = new Date();
-      const date = `${now.getDate()}-${
-        now.getMonth() + 1
-      }-${now.getFullYear()}`;
-      await csv.toDisk(`./posts/${date}.csv`);
-    });
-  console.log(`Saving posts to ${date}.csv`);
+app.set("port", process.env.PORT || 5000);
+app.use(express.static("index"));
+
+app.get("/", function (request, response) {
+  response.send("Collecting hot posts!");
+});
+
+app.listen(app.get("port"), function () {
+  cron.schedule("0 0 * * *", function () {
+    r.getHot()
+      .fetchAll()
+      .filter((post) => post.selftext !== "")
+      .map((post) => {
+        return {
+          id: post.id,
+          author: post.author.name,
+          title: post.title,
+          selfText: post.selftext,
+          date: formatTime(post.created_utc),
+          score: post.score,
+          subreddit: post.subreddit_name_prefixed,
+          numComments: post.num_comments,
+          spoiler: post.spoiler ? true : false,
+          nsfw: post.over_18 ? true : false,
+          isVideo: post.is_video,
+        };
+      })
+      .then(async (posts) => {
+        // const csv = new ObjectsToCsv(posts);
+        const time = formatDateNow(new Date());
+        // await csv.toDisk(`./posts/${time}.csv`);
+        const postsStr = JSON.stringify(posts);
+        var mailOptions = {
+          from: process.env.EMAIL,
+          to: process.env.EMAIL_TO,
+          subject: `Hot Posts for ${time}`,
+          html: "<h1>Hello!</h1><p>Hot posts incoming</p>",
+          attachments: [
+            {
+              filename: `${time}.txt`,
+              content: postsStr,
+            },
+          ],
+        };
+        mail.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Email sent: " + info.response);
+          }
+        });
+      });
+  });
 });
